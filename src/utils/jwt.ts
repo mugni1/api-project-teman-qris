d22a0e6e325ed1c8100290cb6f201ec7ceaacd83
mugni1 <asepabdulmugni22@gmail.com>
2026-01-17 11:47:06 +0700

    feat: init jwt

---
+++ src/utils/jwt.ts
@@ -0,0 +1,33 @@
+import jwt from 'jsonwebtoken';
+import "dotenv/config";
+
+export interface MyJwtPayload {
+    id: string;
+    firstname: string;
+    lastname: string;
+    fullname: string;
+    email: string;
+    iat: number;
+    exp: number;
+}
+const JWT_SECRET_KEY = process.env.JWT_SECRET_KEY as string
+
+export const generateToken = (payload: MyJwtPayload): string => {
+    return jwt.sign({
+        id: payload.id,
+        firstname: payload.firstname,
+        lastname: payload.lastname,
+        fullname: payload.fullname,
+        email: payload.email,
+    },
+        JWT_SECRET_KEY, { expiresIn: '7d' }
+    );
+};
+
+export const decodeToken = (token: string): MyJwtPayload | null => {
+    try {
+        return jwt.verify(token, JWT_SECRET_KEY) as MyJwtPayload;
+    } catch {
+        return null;
+    }
+};
