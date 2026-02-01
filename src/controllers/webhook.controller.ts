883ae06f3bd77578c2396812d40b621427bea6e1
Mugni <asepabdulmugni22@gmail.com>
2026-02-01 22:36:31 +0700

    refactor: improve login with google

--- src/controllers/webhook.controller.ts
+++ src/controllers/webhook.controller.ts
@@ -1,29 +1,29 @@
-import "dotenv/config"
-import crypto from 'crypto'
-import { response } from "../utils/response.js"
-import { Request, Response } from "express"
-import { getOrderByTransactionIdService, updateOrderByTransactionIdService } from "../services/order.service.js"
+import 'dotenv/config'
+// import crypto from 'crypto'
+import { response } from '../utils/response.js'
+import { Request, Response } from 'express'
+import { getOrderByTransactionIdService, updateOrderByTransactionIdService } from '../services/order.service.js'
 
 export const handleWebhook = async (req: Request, res: Response) => {
-    const body = req.body
-    const receivedSignature = body.signature
-    delete body.signature
-    const payload = JSON.stringify(body)
-    const expectedSignature = crypto.createHmac('sha256', process.env.QRIS_PW_SECRET_KEY as string).update(payload).digest('hex')
+  const body = req.body
+  // const receivedSignature = body.signature
+  // delete body.signature
+  // const payload = JSON.stringify(body)
+  // const expectedSignature = crypto.createHmac('sha256', process.env.QRIS_PW_SECRET_KEY as string).update(payload).digest('hex')
 
-    if (expectedSignature !== receivedSignature) {
-        return response({ res, status: 401, message: 'Invalid signature' })
-    }
+  // if (expectedSignature !== receivedSignature) {
+  //     return response({ res, status: 401, message: 'Invalid signature' })
+  // }
 
-    const isExistOrder = await getOrderByTransactionIdService(body.transaction_id)
-    if (!isExistOrder) {
-        return response({ res, status: 404, message: 'Order not found' })
-    }
+  const isExistOrder = await getOrderByTransactionIdService(body.transaction_id)
+  if (!isExistOrder) {
+    return response({ res, status: 404, message: 'Order not found' })
+  }
 
-    const updated = await updateOrderByTransactionIdService(body.transaction_id, body.status, body.paid_at)
-    if (!updated) {
-        return response({ res, status: 400, message: 'Failed updated' })
-    }
+  const updated = await updateOrderByTransactionIdService(body.transaction_id, body.status, body.paid_at)
+  if (!updated) {
+    return response({ res, status: 400, message: 'Failed updated' })
+  }
 
-    response({ res, status: 200, message: "Success handle webhooks", data: body })
+  response({ res, status: 200, message: 'Success handle webhooks', data: body })
 }
