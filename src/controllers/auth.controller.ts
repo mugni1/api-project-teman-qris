007de3f6656d58cd0a6f02751f0bf70fbecf29ba
Mugni <asepabdulmugni22@gmail.com>
2026-02-01 15:29:53 +0700

    feat: add auth google route

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,74 +1,105 @@
-import { Request, Response } from "express"
-import { response } from "../utils/response.js"
-import { loginEmailExistService, registerEmailExistService, registerService } from "../services/auth.service.js"
-import { loginSchema, registerSchema } from "../schema/auth.schema.js"
-import { comparePassword, hashedPassword } from "../utils/bcrypt.js"
-import { generateToken } from "../utils/jwt.js"
+import 'dotenv/config'
+import { Request, Response } from 'express'
+import { response } from '../utils/response.js'
+import { loginEmailExistService, registerEmailExistService, registerService } from '../services/auth.service.js'
+import { loginSchema, registerSchema } from '../schema/auth.schema.js'
+import { comparePassword, hashedPassword } from '../utils/bcrypt.js'
+import { generateToken } from '../utils/jwt.js'
+import { authClient, authGoogleUrl } from '../libs/oauth.js'
+import { google } from 'googleapis'
 
 export const registerController = async (req: Request, res: Response) => {
-    const body = req.body
-    if (!body) {
-        return response({ res, status: 400, message: "Invalid input" })
+  const body = req.body
+  if (!body) {
+    return response({ res, status: 400, message: 'Invalid input' })
+  }
+  const { data, success, error } = registerSchema.safeParse(body)
+  if (!success) {
+    const errors = error.issues.map((err) => ({ message: err.message, path: err.path.join('_') }))
+    return response({ res, status: 400, message: 'Invalid input', errors })
+  }
+  try {
+    data.password = hashedPassword(data.password)
+    data.fullname = `${data.firstname} ${data?.lastname || ''}`.trim()
+    // check email exist
+    const isEmailExist = await registerEmailExistService(data.email)
+    if (isEmailExist) {
+      return response({ res, status: 400, message: 'Failed register email is already exist' })
     }
-    const { data, success, error } = registerSchema.safeParse(body)
-    if (!success) {
-        const errors = error.issues.map((err) => ({ message: err.message, path: err.path.join('_') }))
-        return response({ res, status: 400, message: "Invalid input", errors })
-    }
-    try {
-        data.password = hashedPassword(data.password)
-        data.fullname = `${data.firstname} ${data?.lastname || ""}`.trim()
-        // check email exist
-        const isEmailExist = await registerEmailExistService(data.email)
-        if (isEmailExist) {
-            return response({ res, status: 400, message: "Failed register email is already exist" })
-        }
-        // store to db
-        const result = await registerService(data)
-        response({ res, status: 201, message: "Success register", data: result })
-    } catch {
-        response({ res, status: 500, message: "Internal server error" })
-    }
+    // store to db
+    const result = await registerService(data)
+    response({ res, status: 201, message: 'Success register', data: result })
+  } catch {
+    response({ res, status: 500, message: 'Internal server error' })
+  }
 }
 
 export const loginController = async (req: Request, res: Response) => {
-    const body = req.body
-    if (!body) {
-        return response({ res, status: 400, message: "Invalid input" })
+  const body = req.body
+  if (!body) {
+    return response({ res, status: 400, message: 'Invalid input' })
+  }
+  const { data, success, error } = loginSchema.safeParse(body)
+  if (!success) {
+    const errors = error.issues.map((err) => ({ message: err.message, path: err.path.join('_') }))
+    return response({ res, status: 400, message: 'Invalid input', errors })
+  }
+  try {
+    // find user exsist with email
+    const isEmailExist = await loginEmailExistService(data.email)
+    if (!isEmailExist) {
+      return response({ res, status: 400, message: 'Failed login invalid email or password' })
     }
-    const { data, success, error } = loginSchema.safeParse(body)
-    if (!success) {
-        const errors = error.issues.map((err) => ({ message: err.message, path: err.path.join('_') }))
-        return response({ res, status: 400, message: "Invalid input", errors })
+    // compare password
+    const isValidPassword = await comparePassword(data.password, isEmailExist.password as string)
+    if (!isValidPassword) {
+      return response({ res, status: 400, message: 'Failed login invalid email or password' })
     }
-    try {
-        // find user exsist with email
-        const isEmailExist = await loginEmailExistService(data.email)
-        if (!isEmailExist) {
-            return response({ res, status: 400, message: "Failed login invalid email or password" })
-        }
-        // compare password
-        const isValidPassword = await comparePassword(data.password, isEmailExist.password as string)
-        if (!isValidPassword) {
-            return response({ res, status: 400, message: "Failed login invalid email or password" })
-        }
-        // generate token
-        const token = generateToken({
-            id: isEmailExist.id,
-            email: isEmailExist.email,
-            role: isEmailExist.role,
-            firstname: isEmailExist.firstname,
-            lastname: isEmailExist.lastname || "",
-            fullname: isEmailExist.fullname || ""
-        })
-        isEmailExist.password = null
-        response({
-            res, status: 200, message: "Success login", data: {
-                token,
-                user: isEmailExist
-            }
-        })
-    } catch (err: unknown) {
-        response({ res, status: 500, message: "Internal server error", errors: err })
-    }
+    // generate token
+    const token = generateToken({
+      id: isEmailExist.id,
+      email: isEmailExist.email,
+      role: isEmailExist.role,
+      firstname: isEmailExist.firstname,
+      lastname: isEmailExist.lastname || '',
+      fullname: isEmailExist.fullname || '',
+    })
+    isEmailExist.password = null
+    response({
+      res,
+      status: 200,
+      message: 'Success login',
+      data: {
+        token,
+        user: isEmailExist,
+      },
+    })
+  } catch (err: unknown) {
+    response({ res, status: 500, message: 'Internal server error', errors: err })
+  }
+}
+
+export const googleRedirect = (req: Request, res: Response) => {
+  res.redirect(authGoogleUrl)
+}
+
+export const googleCallback = async (req: Request, res: Response) => {
+  const code = req.query.code as string
+  try {
+    // set credentials
+    const { tokens } = await authClient.getToken(code)
+    authClient.setCredentials(tokens)
+
+    // get user info
+    const oauth2 = google.oauth2({
+      auth: authClient,
+      version: 'v2',
+    })
+    const { data: userInfo } = await oauth2.userinfo.get()
+    res.redirect(
+      `${process.env.FE_ORIGIN_URL}?email=${userInfo.email}&avatar=${userInfo.picture}&name=${userInfo.name}&provider=google`,
+    )
+  } catch {
+    res.status(500).send('Something went wrong')
+  }
 }
--- src/routes/auth.route.ts
+++ src/routes/auth.route.ts
@@ -1,8 +1,10 @@
-import e from "express"
-import { loginController, registerController } from "../controllers/auth.controller.js"
+import e from 'express'
+import { googleCallback, googleRedirect, loginController, registerController } from '../controllers/auth.controller.js'
 
 const router = e.Router()
-router.post("/register", registerController)
-router.post("/login", loginController)
+router.post('/register', registerController)
+router.post('/login', loginController)
+router.get('/auth/google', googleRedirect)
+router.get('/auth/google/callback', googleCallback)
 
 export default router
