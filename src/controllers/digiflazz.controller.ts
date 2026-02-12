4e88d8bcb5bacb90e0a3670958f2f706dff52f5a
Mugni <asepabdulmugni22@gmail.com>
2026-02-12 12:38:24 +0700

    feat: add transaction on digiflazz route

--- src/controllers/digiflazz.controller.ts
+++ src/controllers/digiflazz.controller.ts
@@ -50,3 +50,33 @@ export const priceList = async (req: Request, res: Response) => {
     response({ res, status: 500, message: 'Internal server error' })
   }
 }
+
+export const transaction = async (req: Request, res: Response) => {
+  const ref_id = req.query.ref_id?.toString() || ''
+  if (ref_id == '') {
+    return response({ res, status: 400, message: 'ref_id is required' })
+  }
+  const customer_no = req.query.customer_no?.toString() || ''
+  if (customer_no == '') {
+    return response({ res, status: 400, message: 'customer_no is required' })
+  }
+  const buyer_sku_code = req.query.buyer_sku_code?.toString() || ''
+  if (buyer_sku_code == '') {
+    return response({ res, status: 400, message: 'buyer_sku_code is required' })
+  }
+  const testing = ['true', '1'].includes(String(req.query.testing).toLowerCase())
+  const cb_url = req.query.cb_url?.toString() || ''
+  const apikey = process.env.DIGI_FLAZZ_API_KEY
+  const username = process.env.DIGI_FLAZZ_USERNAME
+  const sign = createHash('md5').update(`${username}${apikey}${ref_id}`).digest('hex')
+  try {
+    const results: AxiosResponse<DigiflazzPriceListResponse> = await axios.post(
+      `${ENDPOINT_EX.digiflazz}/v1/transaction`,
+      { cmd: 'transaction', username, sign, ref_id, buyer_sku_code, customer_no, testing, cb_url },
+    )
+    response({ res, status: 201, message: 'Success create transaction', data: results.data })
+  } catch (err: any) {
+    console.log('DIGIFLAZZ ERROR:', err.response?.data || '')
+    response({ res, status: 500, message: 'Internal server error' })
+  }
+}
--- src/routes/digiflazz.route.ts
+++ src/routes/digiflazz.route.ts
@@ -1,8 +1,9 @@
 import e from 'express'
-import { checkSaldo, priceList } from '../controllers/digiflazz.controller.js'
+import { checkSaldo, priceList, transaction } from '../controllers/digiflazz.controller.js'
 
 const router = e.Router()
 router.get('/check-saldo', checkSaldo)
 router.get('/price-list', priceList)
+router.get('/transaction', transaction)
 
 export default router
