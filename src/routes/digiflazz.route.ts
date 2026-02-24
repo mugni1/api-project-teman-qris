2d0e785a0e72d06e5235a8e5da9e1bf713b183e7
Mugni <asepabdulmugni22@gmail.com>
2026-02-24 13:03:27 +0700

    refactor: add middleware protect in route

--- src/routes/digiflazz.route.ts
+++ src/routes/digiflazz.route.ts
@@ -1,9 +1,11 @@
 import e from 'express'
 import { checkSaldo, priceList, transaction } from '../controllers/digiflazz.controller.js'
+import { authMiddleware } from '../middlewares/auth.middleware.js'
+import { superUserMiddleware } from '../middlewares/super-user-only.middleware.js'
 
 const router = e.Router()
-router.get('/check-saldo', checkSaldo)
-router.get('/price-list', priceList)
-router.get('/transaction', transaction)
+router.get('/check-saldo', authMiddleware, superUserMiddleware, checkSaldo)
+router.get('/price-list', authMiddleware, superUserMiddleware, priceList)
+router.get('/transaction', authMiddleware, superUserMiddleware, transaction)
 
 export default router
--- src/routes/vip.route.ts
+++ src/routes/vip.route.ts
@@ -1,9 +1,11 @@
 import e from 'express'
 import { checkNickname, getPrepaidService, getProfile } from '../controllers/vip.controller.js'
+import { authMiddleware } from '../middlewares/auth.middleware.js'
+import { superUserMiddleware } from '../middlewares/super-user-only.middleware.js'
 
 const router = e.Router()
-router.get('/profile', getProfile)
-router.get('/prepaid', getPrepaidService)
+router.get('/profile', authMiddleware, superUserMiddleware, getProfile)
+router.get('/prepaid', authMiddleware, superUserMiddleware, getPrepaidService)
 router.post('/nickname', checkNickname)
 
 export default router
